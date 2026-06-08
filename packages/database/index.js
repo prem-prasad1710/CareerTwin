81d1272fcc9c8d7d3cd73b676ab7dcaa09e7bbd2
Prem Prasad <prem.prasad@paytm.com>
2026-06-08 12:40:34 +0530

    issue fixed!

---
+++ packages/database/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { PrismaClient } = require('@prisma/client');
+
+const globalForPrisma = globalThis;
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+module.exports = {
+  prisma,
+  db: prisma,
+  ...require('@prisma/client'),
+};
